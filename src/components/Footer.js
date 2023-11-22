1c7544e6ce359832e3e4350a909667d082a6fc87
Adapaanjani <104987924+Adapaanjani@users.noreply.github.com>
2023-11-23 00:19:38 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,10 @@ import './Footer.css'; // Import your custom styles
 const Footer = () => {
   return (
     <footer className="footer">
-      <h4>&copy; 2023 అŋɬɧąཞıక్షơ. All rights reserved.</h4>
+      <h4>&copy; 2023 అŋɬɧąཞıక్షơ. All rights reserved By <a href="https://www.linkedin.com/in/anjani-naga-sai-satya-sri-adapa-520236223/">Anjani</a>.</h4>
     </footer>
   );
 };
 
 export default Footer;
+
